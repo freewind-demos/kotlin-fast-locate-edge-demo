28d834f6f5b651ac466d068f65c2642818910002
freewind <nowindlee@gmail.com>
2019-01-14 12:53:33 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Kotlin Hello World Demo
-=======================
+Kotlin Fast Locate Edge Demo
+============================
+
+使用二分法快速定位一个内部边界。
 
 Run `Hello.kt` in your IDE.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,26 @@
 package example
 
-fun main(args: Array<String>) {
-    println(hello("Kotlin"))
+fun findEdge(list: List<Int>): Int {
+    var start = 0
+    var end = list.size
+
+    while (true) {
+        if (end == start + 1)
+            return start
+        val middle = (end + start) / 2
+        if (list[middle] == 0) {
+            end = middle
+        } else {
+            start = middle
+        }
+    }
 }
 
-fun hello(name: String): String = "Hello, $name!"
+fun main(args: Array<String>) {
+    val list = mutableListOf<Int>()
+    (0..12000).toList().forEach { list.add(1) }
+    (0..30009).toList().forEach { list.add(0) }
+
+    val edge = findEdge(list)
+    println(edge)
+}
